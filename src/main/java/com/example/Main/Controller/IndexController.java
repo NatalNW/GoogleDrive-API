1adbe958e340792c56712e566ebe18f2167d9e4f
natanael.lima <natanael.lima@tivit.com>
2019-04-26 14:35:16 -0300

    V3.0 | New version using Maven

--- src/main/java/com/example/Main/Controller/IndexController.java
+++ src/main/java/com/example/Main/Controller/IndexController.java
@@ -1,10 +1,10 @@
-package com.example.Controller;
+package com.example.Main.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class indexController {
+public class IndexController {
 	
 	@RequestMapping("/")
 	public String index() {
