1b8a3c67d099cc7750aafb04de7ea32b37ef0feb
NatalNW <natanaelweslley1@gmail.com>
2020-07-15 01:04:34 -0300

    making some code for fun hahaha

--- src/main/java/com/googledrive/Config/GoogleDriveConfig.java
+++ src/main/java/com/googledrive/Config/GoogleDriveConfig.java
@@ -14,8 +14,10 @@ import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.drive.Drive;
 import com.google.api.services.drive.DriveScopes;
 import com.google.api.services.drive.model.File;
+import com.google.api.services.drive.model.FileList;
 import com.google.api.services.drive.model.Permission;
 
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -24,12 +26,13 @@ import java.security.GeneralSecurityException;
 import java.util.Collections;
 import java.util.List;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.multipart.MultipartFile;
 
 @Configuration
 public class GoogleDriveConfig {
-	private static final String APPLICATION_NAME = "ProjectLostPets";
+	private static final String APPLICATION_NAME = "mydrive-api";
 	private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
 	private static final String TOKENS_DIRECTORY_PATH = "tokens";
 	private static final List<String> SCOPES = Collections.singletonList(DriveScopes.DRIVE);
@@ -38,99 +41,31 @@ public class GoogleDriveConfig {
 	private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
 		// Load client secrets.
 		InputStream in = GoogleDriveConfig.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
+		
+		if (in == null) {
+			throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
+		}
+
 		GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
 
 		// Build flow and trigger user authorization request.
-		GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(HTTP_TRANSPORT, JSON_FACTORY,clientSecrets, SCOPES)
+		GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
 			.setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
 			.setAccessType("offline")
 			.build();
 
-		LocalServerReceiver receiver = new LocalServerReceiver.Builder()
-			.setPort(8888)
-			.build();
-
+		LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(9898).build();
+		
 		return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
 	}
 
-	public static void uploadFile(MultipartFile file) throws IOException, GeneralSecurityException {
-		java.io.File image = convert(file);
-		Drive service = getService();
-		
-		File fileMetadata = new File();
-		fileMetadata.setName(image.getName());
-		fileMetadata.setMimeType("image/*");
-		
-		java.io.File filePath = new java.io.File(image.getAbsolutePath());
-		FileContent mediaContent = new FileContent("image/*", filePath);
-		File imgFile = service.files().create(fileMetadata, mediaContent).setFields("id").execute();
-
-		// createPublicPermission(imgFile.getId());
-
-		System.out.println("IMG File ID: " + imgFile.getId());
-	}
-
-	// public static void listFiles(Drive service) throws IOException {
-	// // Print the names and IDs for up to 10 files.
-	// FileList result =
-	// service.files().list().setPageSize(10).setFields("nextPageToken, files(id,
-	// name)").execute();
-	// List<File> files = result.getFiles();
-	// if (files == null || files.isEmpty()) {
-	// System.out.println("No files found.");
-	// } else {
-	// System.out.println("Files:");
-	// for (File file : files) {
-	// System.out.printf("%s (%s)\n", file.getName(), file.getId());
-	// }
-	// }
-
-	// }
-
-	public static Permission createPublicPermission(String googleFileId) throws IOException, GeneralSecurityException {
-		// All values: user - group - domain - anyone
-		String permissionType = "anyone";
-		// All values: organizer - owner - writer - commenter - reader
-		String permissionRole = "reader";
-
-		Permission newPermission = new Permission();
-		newPermission.setType(permissionType);
-		newPermission.setRole(permissionRole);
-
-		Drive driveService = getService();
-
-		return driveService.permissions().create(googleFileId, newPermission).execute();
-	}
-
-	private static java.io.File convert(MultipartFile file) throws IOException {
-		java.io.File convFile = new java.io.File(file.getOriginalFilename());
-		convFile.createNewFile();
-		FileOutputStream fos = new FileOutputStream(convFile);
-		fos.write(file.getBytes());
-		fos.close();
-		return convFile;
-	}
-
+	@Bean
 	public static Drive getService() throws GeneralSecurityException, IOException {
 		// Build a new authorized API client service.
 		final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
 
-		return service = new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
-				.setApplicationName(APPLICATION_NAME).build();
+		return new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+			.setApplicationName(APPLICATION_NAME)
+			.build();
 	}
-
-	// public static void main(String... args) throws IOException,
-	// GeneralSecurityException {
-	//
-	// JFileChooser fc = new JFileChooser();
-	// int res = fc.showOpenDialog(null);
-	//
-	// if (res == JFileChooser.APPROVE_OPTION) {
-	// java.io.File arquivo = fc.getSelectedFile();
-	// UploadFile(arquivo, getService());
-	// } else
-	// System.out.println("Voce nao selecionou o arquivo.");
-	//
-	// listFiles(getService());
-	// }
 }
--- src/main/java/com/googledrive/Controller/IndexController.java
+++ src/main/java/com/googledrive/Controller/IndexController.java
@@ -2,26 +2,52 @@ package com.googledrive.Controller;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
+import java.util.ArrayList;
+import java.util.List;
 
-import org.springframework.stereotype.Controller;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.google.api.services.drive.model.File;
 import com.googledrive.Config.GoogleDriveConfig;
 
-@Controller
+@RestController
 public class IndexController {
 
-	@RequestMapping(value = "/upload-image", method = RequestMethod.POST)
-	public String uploadImages(Model model, @RequestParam(value = "files") MultipartFile[] files) throws IOException, GeneralSecurityException{
+	// private JSONObject obj;
 
-		for (MultipartFile file : files) {
-			GoogleDriveConfig.uploadFile(file);
+	@RequestMapping(value = "/upload-image", method = RequestMethod.POST)
+	public File uploadImage(Model model, @RequestParam(value = "image") MultipartFile image) throws IOException, GeneralSecurityException {
+		File img = GoogleDriveConfig.uploadImage(image);
+
+		return img;
+	}
+
+	@RequestMapping(value = "/upload-images", method = RequestMethod.POST)
+	public String uploadImages(Model model, @RequestParam(value = "images") MultipartFile[] images) throws IOException, GeneralSecurityException {
+		List<File> imgs = new ArrayList<>();
+		// obj = new JSONObject();
+
+		for (MultipartFile image : images) {
+			imgs.add(GoogleDriveConfig.uploadImage(image));
 		}
-		
-		return "upload";
-	}	
+
+		// for (File img : imgs) {}
+
+		return "";
+	}
+
+	@RequestMapping(value = "/files", method = RequestMethod.GET)
+	public List<File> listFiles() throws IOException, GeneralSecurityException {
+		List<File> files = GoogleDriveConfig.listFiles();
+
+		return files;
+	}
 }
